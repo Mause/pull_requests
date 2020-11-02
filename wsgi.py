5b1286618c828224754b73593edf8d410979f4ac
Elliana May <me@mause.me>
2020-11-02 16:03:00 +0800

    Sentry

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,14 @@
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
 from app import create_app
+
+if 'SENTRY_DSN' in os.environ:
+    sentry_sdk.init(
+        os.environ['SENTRY_DSN'],
+        integrations=[FlaskIntegration()],
+        release=os.environ['HEROKU_SLUG_COMMIT'],
+        traces_sample_rate=0.75,
+    )
 
 app = create_app()
