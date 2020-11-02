8cd0cedd7047e8d171a25a3071c83c179cf627fe
Elliana <me@mause.me>
2020-11-02 16:52:43 +0800

    Add proxy fix

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 import os
+
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app import create_app
 
@@ -13,3 +15,4 @@ if 'SENTRY_DSN' in os.environ:
     )
 
 app = create_app()
+app = ProxyFix(app, x_for=1, x_host=1)
